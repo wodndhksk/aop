dbfb7c7ff40ffce9c802cbf66777f6f30097edc2
Jaewoo Kim <wodndhksk@gmail.com>
2024-03-17 22:56:32 +0900

    재시도 AOP

--- src/main/java/hello/aop/exam/ExamRepository.java
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public class ExamRepository {
      * 5번에 한번 실패하는 요청
      */
     @Trace
+    @Retry(4) // Retry 에노테이션의 int value()의 값을 4로 변경 (디폴트는 3으로 설정)
     public String save(String itemId) {
         seq++;
         if(seq % 5 == 0) {
